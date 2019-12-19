5e1c356bb7667f1e1fad58035cb110a37bfc7e75
Charles <chadalton@paypal.com>
2019-12-18 17:42:26 -0800

    Complete 2.1

---
+++ chapter-2/2.1.scm
@@ -0,0 +1,49 @@
+;;exercise 2.1-completed!
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(define (make-rat n d)
+  (let ((g (gcd (abs n) (abs d))))
+    (if (> 0 d)
+	(cons (/ (- n) g) (/ (abs d) g))
+	(cons (/ n g) (/ d g)))))
+
+(define (numer x) (car x))
+
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+	       (* (numer y) (denom x)))
+	    (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+	       (* (numer y) (denom x)))
+	    (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+	    (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+	    (* (denom x) (numer y))))
+
+(define one-third (make-rat 1 3))
+
+(define two-thirds (make-rat 5 3))
+
+(define test (add-rat two-thirds one-third))
+
+(print-rat test)
+
