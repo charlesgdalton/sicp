b3838231a109f85540e7f23770b103abfe46eb82
Charles <chadalton@paypal.com>
2020-01-16 14:52:15 -0800

    Complete 2.23

---
+++ chapter-2/2.23.scm
@@ -0,0 +1,13 @@
+;;exercise 2.23-completed!
+
+
+(define (for-each proc items)
+  (cond ((null? items) true)
+	(else
+	 (proc (car items))
+	 (for-each proc (cdr items)))))
+
+(for-each (lambda (x)
+	    (newline)
+	    (display x))
+	  (list 57 321 88))
