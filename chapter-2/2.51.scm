2955b0ea0f29f5cbf08ec6ae10063a23d74b706f
Charles <chadalton@paypal.com>
2020-02-27 15:40:35 -0800

    Complete 2.50 and 2.51

---
+++ chapter-2/2.51.scm
@@ -0,0 +1,22 @@
+;;exercise 2.51-complete!
+
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-bot
+	   (transform-painter
+	    painter1
+	    (make-vect 0.0 0.0)
+	    (make-vect 1.0 0.0)
+	    split-point))
+	  (paint-top
+	   (transform-painter
+	    painter2
+	    split-point
+	    (make-vect 1.0 0.5)
+	    (make-vect 0.0 1.0))))
+      (lambda (frame)
+	(paint-bot frame)
+	(paint-top frame)))))
+
+(define (below painter1 painter2)
+  (rotate90 (beside (rotate270 painter1) (rotate270 painter2))))
